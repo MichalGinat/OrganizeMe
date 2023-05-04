4eb7b82fcbef39010ef64993ba6d39b9b891cb04
MichalGinat <michalginat83@gmail.com>
2023-05-04 18:33:14 +0300

    add validation for register and signIn

--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,19 +1,35 @@
 import { useState } from 'react';
 import { auth} from '../firebase-config.js';
-import {createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
 
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
       const { user } = await createUserWithEmailAndPassword(auth, email, password);
       console.log(user);
+      setError(null);
+      setSuccess(true);
     } catch (error) {
-      console.log(error);
+      const errorCode = error.code;
+      const errorMessage = firebaseErrorMessages[errorCode] || error.message;
+      setError(errorMessage);
+      setSuccess(false);
     }
+  } 
+
+  // to display custom error messages for firebase errors
+  const firebaseErrorMessages = {
+    'auth/email-already-in-use': 'Email is already registered. Try another email.',
+    'auth/invalid-email': 'Invalid email format. Please enter a valid email.',
+    'auth/missing-password': 'Please enter a password.',
+    'auth/weak-password': 'Password must be at least 6 characters long.',
+    'auth/network-request-failed': 'Network error. Please check your internet connection and try again.',
   }
 
   return (
@@ -23,6 +39,8 @@ function Register() {
       <label>Password</label>
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       <button type="submit">Register</button>
+      {error && <p style={{color: 'red'}}>{error}</p>}
+      {success && !error && <p style={{color: 'green'}}>Signed up successfully!</p>}
     </form>
   );
 }
--- client/src/components/signIn.jsx
+++ client/src/components/signIn.jsx
@@ -1,21 +1,35 @@
 import { useState } from 'react';
 import { auth } from '../firebase-config.js';
-import {signInWithEmailAndPassword } from "firebase/auth";
-import { useNavigate} from 'react-router-dom';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
+    if (!email || !password) {
+      setErrorMessage('Please enter your email and password.');
+      return;
+    }
     try {
       await signInWithEmailAndPassword(auth, email, password);
       navigate("/Home");
 
     } catch (error) {
-      console.log(error);
+      switch (error.code) {
+        case 'auth/user-not-found':
+          setErrorMessage("Sorry, we couldn't find an account with that email address. Please double-check your email and try again.");
+          break;
+        case 'auth/wrong-password':
+          setErrorMessage('Oops! The password you entered is incorrect. Please try again.');
+          break;
+        default:
+          setErrorMessage('Sorry, something went wrong. Please try again later.');
+      }
     }
   };
 
@@ -26,8 +40,10 @@ function SignIn() {
       <label>Password</label>
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       <button type="submit">Login</button>
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
     </form>
   );
 }
 
 export default SignIn;
+
