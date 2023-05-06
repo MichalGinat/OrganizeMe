c83d1453f6c231e815124febfc37633c57e4f0c8
MichalGinat <michalginat83@gmail.com>
2023-05-06 22:11:19 +0300

    client+server port response

--- server/src/server.js
+++ server/src/server.js
@@ -7,10 +7,16 @@ dotenv.config({ path: './server/src/.env' });
 const app = express();
 
 connectToDb(() => {
-  app.listen(process.env.PORT || 3000, () => {
-    console.log("Server(Express And Mongo) started on port 3000");
+  app.listen(PORT, () => {
+    console.log("Server (Express and Mongo) started on port 3000");
   });
 });
 
+app.get('/api', (req, res) => {
+  console.log(`Request received from port 3000`);
+  res.json({ "users": ["userOne", "userTwo"] });
+});
 
-
+app.listen(PORT, () => {
+  console.log(`Server listening on port 3000`);
+});
