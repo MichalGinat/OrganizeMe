1f5819f322ea8f729d359f1f59272b4e705e0c54
MichalGinat <michalginat83@gmail.com>
2023-05-29 17:57:05 +0300

    calendar page changes

--- client/src/components/TaskModal.jsx
+++ client/src/components/TaskModal.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaCheckCircle, FaExclamationCircle } from 'react-icons/fa';
 
-const TaskModal = ({ task, onClose, onEdit, onRemove }) => {
+const TaskModal = ({ task, onClose, onEdit, onRemove, onComplete, successMessage, errorMessage}) => {
+
+  
   const { title, category, importance, comments, status } = task;
 
   const handleEditClick = () => {
+
     onEdit(task);
   };
 
@@ -12,9 +16,13 @@ const TaskModal = ({ task, onClose, onEdit, onRemove }) => {
     onRemove(task);
   };
 
+  const handleCompleteClick = () => {
+    onComplete(task);
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-75 z-50">
-      <div className="bg-white rounded-lg p-6 text-center relative">
+    <div className="bg-white rounded-lg p-6 text-center relative">
         <button
           className="closeButton absolute top-0 left-0 mt-4 ml-4 text-gray-500 hover:text-gray-700 font-bold"
           onClick={onClose}
@@ -36,7 +44,7 @@ const TaskModal = ({ task, onClose, onEdit, onRemove }) => {
         </p>
         <div className="flex justify-center mt-6">
           <button
-            className="bg-purple-500 hover:bg-purple-600 text-white py-2 px-4 rounded mr-2"
+            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mr-2"
             onClick={handleEditClick}
           >
             Edit Task
@@ -47,23 +55,32 @@ const TaskModal = ({ task, onClose, onEdit, onRemove }) => {
           >
             Remove Task
           </button>
+          <button
+            className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded ml-2"
+            onClick={handleCompleteClick}
+          >
+            Done Task
+          </button>
         </div>
+        {successMessage && (
+          <div className="flex bg-green-100 rounded-lg p-4 mt-4 text-sm text-green-700" role="alert">
+            <FaCheckCircle className="mr-2" />
+            <div>
+              <span className="font-medium">Success!</span> {successMessage}
+            </div>
+          </div>
+        )}
+        {errorMessage && (
+          <div className="flex bg-red-100 rounded-lg p-4 mt-4 text-sm text-red-700" role="alert">
+            <FaExclamationCircle className="mr-2" />
+            <div>
+              <span className="font-medium">Error!</span> {errorMessage}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
-};
-
-TaskModal.propTypes = {
-  task: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    category: PropTypes.string.isRequired,
-    importance: PropTypes.string.isRequired,
-    comments: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-  }).isRequired,
-  onClose: PropTypes.func.isRequired,
-  onEdit: PropTypes.func.isRequired,
-  onRemove: PropTypes.func.isRequired,
 };
 
 export default TaskModal;
--- client/src/pages/CalendarPage.jsx
+++ client/src/pages/CalendarPage.jsx
@@ -6,6 +6,10 @@ import PropTypes from 'prop-types';
 import TaskForm from '../components/TaskForm.jsx';
 import CalendarTask from '../components/CalendarTask.jsx';
 import TaskModal from '../components/TaskModal.jsx';
+import EditTask from '../components/EditTask.jsx';
+
+import { FaCheckCircle, FaExclamationCircle } from 'react-icons/fa';
+
 
 CalendarPage.propTypes = {
   userId: PropTypes.string.isRequired,
@@ -15,6 +19,11 @@ function CalendarPage(props) {
   const [events, setEvents] = useState([]);
   const [showTaskForm, setShowTaskForm] = useState(false);
   const [selectedTask, setSelectedTask] = useState(null);
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [editTask, setEditTask] = useState(null);
+  
 
   useEffect(() => {
     fetchTasksByDate(props.userId);
@@ -29,9 +38,12 @@ function CalendarPage(props) {
       const tasks = await response.json();
       const events = tasks.map((task) => {
         return {
+          taskId: task.taskId,
           title: task.title,
+          taskName: task.taskName,
           start: new Date(task.start),
           end: new Date(task.end),
+          dueDate: task.dueDate,
           category: task.category,
           importance: task.importance,
           comments: task.comments,
@@ -74,6 +86,40 @@ function CalendarPage(props) {
     };
   };
 
+
+
+  const handleTaskFormSubmit = async (formData) => {
+    try {
+      const response = await fetch('/api/user/AddTask', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: props.userId,
+          tasks: formData,
+        }),
+      });
+
+      if (response.ok) {
+        fetchTasksByDate(props.userId); // Pass the userId parameter here
+        setShowTaskForm(false);
+        setSuccessMessage('Task saved successfully');
+        setTimeout(() => {
+          setSuccessMessage('');
+        }, 3000);
+      } else {
+        setErrorMessage('Failed to add task');
+      }
+    } catch (error) {
+      console.error('Error adding task:', error);
+    }
+  };
+
+  const handleModalClose = () => {
+    setSelectedTask(null);
+  };
+
   const handleEventClick = (event) => {
     setSelectedTask(event);
   };
@@ -86,13 +132,111 @@ function CalendarPage(props) {
     setShowTaskForm(false);
   };
 
-  const handleTaskFormSubmit = (taskData) => {
-    console.log(taskData);
+  const handleEditClick = (task) => {
+  setEditTask(task);
+  setSelectedTask(null);
+  setIsEditModalOpen(true);
+};
+
+
+  const handleEditModalClose = () => {
+    setIsEditModalOpen(false);
   };
 
-  const handleModalClose = () => {
-    setSelectedTask(null);
+  const handleRemoveTask = (task) => {
+    const taskId = task.taskId;
+
+    if (window.confirm('Are you sure you want to remove this task?')) {
+      fetch(`/api/tasks/DeleteTask/${taskId}?userId=${props.userId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+              setEvents((prevEvents) =>
+              prevEvents.filter((event) => event.taskId !== taskId)
+            );
+            setSuccessMessage('Task successfully deleted.'); 
+          } else {
+            setErrorMessage('Failed to delete the task.');
+          }
+        })
+        .catch((error) => {
+          setErrorMessage('Error deleting the task.');
+        })
+        .finally(() => {
+          // Clear the success and error messages after a few seconds
+          setTimeout(() => {
+            setSuccessMessage('');
+            setErrorMessage('');
+          }, 3000);
+        });
+    }
   };
+
+  
+  const handleCompleteTask = (task) => {
+    const taskId = task.taskId;
+
+    if (window.confirm('Are you sure you have finished this task?')) {
+      fetch(`/api/tasks/CompleteTask/${taskId}?userId=${props.userId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            setEvents((prevEvents) =>
+              prevEvents.map((event) =>
+                event.taskId === taskId ? { ...event, status: 'Done' } : event
+              )
+            );
+            setSuccessMessage('Task marked as completed.');
+            setTimeout(() => {
+              setSuccessMessage('');
+            }, 3000);
+          } else {
+            setErrorMessage('Failed to mark the task as completed.');
+          }
+        })
+        .catch((error) => {
+          setErrorMessage('Error marking the task as completed.');
+        })
+        
+    }
+  };
+
+  const handleSaveTask = async (updatedTask) => {
+    try {
+      const response = await fetch(`/api/tasks/UpdateTask/${updatedTask.taskId}?userId=${props.userId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(updatedTask),
+      });
+  
+      if (response.ok) {
+        const updatedEvents = events.map((event) =>
+          event.taskId === updatedTask.taskId ? { ...event, ...updatedTask } : event
+        );
+        setEvents(updatedEvents);
+        setSuccessMessage('Task successfully updated.');
+        fetchTasksByDate(props.userId); // Fetch the updated tasks after updating a task
+        setTimeout(() => {
+          setSuccessMessage('');
+        }, 3000);
+      } else {
+        setErrorMessage('Failed to update the task.');
+      }
+    } catch (error) {
+      setErrorMessage('Error updating the task.');
+    }
+  };
+  
 
   const CustomEvent = ({ event }) => {
     return (
@@ -104,16 +248,65 @@ function CalendarPage(props) {
 
   return (
     <div>
-      <div className="relative">
-        {showTaskForm && (
-          <div className="absolute top-0 left-0 mt-10 ml-10 z-10">
-            <div className="bg-white border border-gray-300 p-4 shadow">
-              <TaskForm onSubmit={handleTaskFormSubmit} onClose={handleCloseTaskForm} />
-            </div>
+        <div className="relative">
+
+        <div className="container mx-auto">
+        <form className="mb-8">
+        </form>
+        {successMessage && !errorMessage && (
+          <div className="alert success flex items-center text-green-500 justify-center mb-4">
+            <FaCheckCircle className="mr-2" />
+            {successMessage}
+          </div>
+        )}
+        {errorMessage && !successMessage && (
+          <div className="alert error flex items-center text-red-500 justify-center mb-4">
+            <FaExclamationCircle className="mr-2" />
+            {errorMessage}
           </div>
         )}
       </div>
 
+        {showTaskForm && (
+          <div className="absolute top-0 left-0 mt-10 ml-10 z-10">
+           
+              <TaskForm onSubmit={handleTaskFormSubmit} onClose={handleCloseTaskForm} />
+            
+          </div>
+        )}
+  
+
+        {selectedTask && (
+          <TaskModal
+            task={selectedTask}
+            onClose={handleModalClose}
+            onRemove={handleRemoveTask} 
+            onEdit={handleEditClick}
+            onComplete={handleCompleteTask}
+            successMessage={successMessage}
+            errorMessage={errorMessage}
+          />
+        )}
+
+       
+
+        {isEditModalOpen && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="bg-white border border-gray-300 p-4 shadow relative z-10">
+              <EditTask
+                task={editTask}
+                onSave={handleSaveTask}
+                onClose={handleEditModalClose}
+                isModalOpen={isEditModalOpen}
+                userId={props.userId}
+                successMessage={successMessage}
+                errorMessage={errorMessage}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+  
       <div className="flex justify-end mt-5 mr-8">
         <button
           onClick={handleAddTaskClick}
@@ -122,30 +315,40 @@ function CalendarPage(props) {
           Add Task
         </button>
       </div>
-
-      <div className="mt-5">
-        <Calendar
-          localizer={localizer}
-          events={events}
-          startAccessor="start"
-          endAccessor="end"
-          className="max-w-full mx-auto"
-          style={{ height: '580px', margin: '30px' }}
-          views={[Views.MONTH, Views.AGENDA]}
-          components={{
-            event: CustomEvent,
-            agenda: {
-              event: CustomEvent,
-            },
-          }}
-          eventPropGetter={eventStyleGetter}
-          popup
-          selectable
-          showMultiDayTimes
-        />
+  
+      <div className="max-w-2xl mx-auto p-4 bg-white border border-gray-300 shadow-md mt-4 flex items-center">
+        <div className="bg-green-500 w-3 h-3 inline-block rounded-full mr-2"></div>
+        <span className="flex-1">Tasks that are done</span>
+        <div className="bg-blue-500 w-3 h-3 inline-block rounded-full mx-2"></div>
+        <span className="flex-1">Tasks that are active</span>
+        <div className="bg-red-500 w-3 h-3 inline-block rounded-full ml-2"></div>
+        <span className="flex-1"> Tasks that are not finished</span>
       </div>
 
-      {selectedTask && <TaskModal task={selectedTask} onClose={handleModalClose} />}
+      {!isEditModalOpen && (
+        <div className="mt-5">
+          <Calendar
+            localizer={localizer}
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            className="max-w-full mx-auto"
+            style={{ height: '580px', margin: '30px' }}
+            views={['month', 'agenda']}
+            components={{
+              event: CustomEvent,
+              agenda: {
+                event: CustomEvent,
+              },
+            }}
+            eventPropGetter={eventStyleGetter}
+            popup
+            selectable
+            showMultiDayTimes
+          />
+        </div>
+      )}
+
     </div>
   );
 }
