2905c61f9bb480fc291179d65e687a24121b3579
MichalGinat <michalginat83@gmail.com>
2023-06-08 20:50:20 +0300

    add change

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,7 +10,7 @@ export default {
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000",
+        target: import.meta.env.VITE_API_BASE_URL,
         changeOrigin: true,
         secure: false,
         ws: true,
