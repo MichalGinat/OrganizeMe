e78d5a27a8de303b83543fccb6f6d563e8ffb4d1
MichalGinat <michalginat83@gmail.com>
2023-06-08 21:16:07 +0300

    vite config change

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,45 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+// export default {
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: import.meta.env.VITE_API_BASE_URL,
+//         changeOrigin: true,
+//         secure: false,
+//         ws: true,
+//       },
+//     },
+//   },
+// };
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-export default {
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+  const apiUrl = isProduction
+    ? "https://organizeme.vercel.app"
+    : "http://localhost:3000";
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: apiUrl,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
       },
     },
-  },
-};
+  };
+});
+
