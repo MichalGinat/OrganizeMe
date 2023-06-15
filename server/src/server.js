90403d162de38825fe29d10b86e42fd229624bc3
MichalGinat <michalginat83@gmail.com>
2023-06-15 20:59:09 +0300

    change dist

--- server/src/server.js
+++ server/src/server.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
 
 app.use(
   "/assets",
-  express.static(path.join(__dirname, "..", "..", "client", "client/dist", "assets"))
+  express.static(path.join(__dirname, "..", "..", "client", "dist", "assets"))
 );
 app.use(express.static(path.join(__dirname, "..", "client", "dist")));
 
